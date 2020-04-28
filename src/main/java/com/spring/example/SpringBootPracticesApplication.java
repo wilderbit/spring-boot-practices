932c3512b4ddfd8629c480a7fabf31f6abbed961
AbrarNitk <abrar.nitk@gmail.com>
2020-04-28 09:45:44 +0530

    init

---
+++ src/main/java/com/spring/example/SpringBootPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.spring.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracticesApplication.class, args);
+	}
+
+}
