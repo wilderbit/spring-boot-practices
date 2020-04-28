cb5e9c3ed8cbc04f630a646d07a386f815287c86
AbrarNitk <abrar.nitk@gmail.com>
2020-04-28 12:32:24 +0530

    add log4j2

--- docs/dependencies_reqirements.md
+++ docs/dependencies_reqirements.md
@@ -1,11 +1,32 @@
 ## Adding log4j
 Maven dependency
 ```xml
-<!-- https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j -->
-<dependency>
-    <groupId>org.apache.logging.log4j</groupId>
-    <artifactId>log4j</artifactId>
-    <version>2.8.2</version>
-</dependency>
+<dependencies>
+    <!-- https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j -->
+
+    <dependency>
+        <groupId>org.apache.logging.log4j</groupId>
+        <artifactId>log4j</artifactId>
+        <version>2.8.2</version>
+    </dependency>
+    
+    <!--OR
+      If using this so exclude logging from spring boot starter,
+      This is recommended 
+    -->
+
+    <dependency>
+      <groupId>org.apache.logging.log4j</groupId>
+      <artifactId>log4j-api</artifactId>
+      <version>2.13.2</version>
+    </dependency>
+
+    <dependency>
+      <groupId>org.apache.logging.log4j</groupId>
+      <artifactId>log4j-core</artifactId>
+      <version>2.13.2</version>
+    </dependency>
+
+</dependencies>
 ``` 
 
--- src/main/java/com/spring/example/controller/ItemController.java
+++ src/main/java/com/spring/example/controller/ItemController.java
@@ -5,6 +5,8 @@ import com.spring.example.pojo.response.Response;
 import com.spring.example.pojo.response.Success;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,13 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/v1")
 public class ItemController {
 
+    final Logger logger = LogManager.getLogger(ItemController.class.getName());
+
     @PostMapping(value = "/api/item/create")
     public ResponseEntity<Response> createItem(@Valid @NotNull @RequestBody ItemRequest item) {
+        logger.info("create_item_call");
         return Success.ok(item);
     }
 
     @GetMapping(value = "/api/item/get")
     public ResponseEntity<Response> getItem() {
+        logger.info("get_item_call");
         return Success.ok(new ItemRequest());
     }
 }
