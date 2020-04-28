4e89c9aaf8ea6a9f2abae0f81462bcba028a44a9
AbrarNitk <abrar.nitk@gmail.com>
2020-04-28 11:25:41 +0530

    add item request class

--- src/main/java/com/spring/example/controller/ItemController.java
+++ src/main/java/com/spring/example/controller/ItemController.java
@@ -1,10 +1,14 @@
 package com.spring.example.controller;
 
+import com.spring.example.pojo.request.ItemRequest;
 import com.spring.example.pojo.response.Response;
 import com.spring.example.pojo.response.Success;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class ItemController {
 
     @PostMapping(value = "/api/item/create")
-    public ResponseEntity<Response> createItem() {
-        return Success.ok("item created");
+    public ResponseEntity<Response> createItem(@Valid @NotNull @RequestBody ItemRequest item) {
+        return Success.ok(item);
     }
 
     @GetMapping(value = "/api/item/get")
--- src/main/java/com/spring/example/pojo/request/ItemRequest.java
+++ src/main/java/com/spring/example/pojo/request/ItemRequest.java
@@ -1,5 +1,19 @@
 package com.spring.example.pojo.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ItemRequest {
 
+    @JsonProperty(value = "name")
+    private String name;
+    @JsonProperty(value = "price")
+    private Double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
 }
